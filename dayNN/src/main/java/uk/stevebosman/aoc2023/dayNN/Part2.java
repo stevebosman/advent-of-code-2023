cea916d0ecb2a8ce084c5d272a0d560d4eddecb1
Steve Bosman <stevebosman@gmail.com>
2023-12-06 19:54:19 +0000

    template day

---
+++ dayNN/src/main/java/uk/stevebosman/aoc2023/dayNN/Part2.java
@@ -0,0 +1,15 @@
+package uk.stevebosman.aoc2023.dayNN;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
+
+public class Part2 {
+  public static void main(final String[] args) throws IOException {
+    try (final Stream<String> lines = Files.lines(Path.of(args[0]))) {
+      System.out.println(LocalDateTime.now() + ": ");
+    }
+  }
+}
