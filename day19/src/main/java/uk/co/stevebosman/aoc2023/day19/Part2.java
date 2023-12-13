c0e380262a8370d642cab7fac59c1a228a77e40f
Steve Bosman <stevebosman@gmail.com>
2023-12-13 21:24:44 +0000

    templates for future days

---
+++ day19/src/main/java/uk/co/stevebosman/aoc2023/day19/Part2.java
@@ -0,0 +1,24 @@
+package uk.co.stevebosman.aoc2023.dayNN;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+
+public class Part2 {
+  public static void main(final String[] args) throws IOException {
+    final Instant start = Instant.now();
+    try {
+      runIt(Path.of(args[0]));
+    } finally {
+      final Instant end = Instant.now();
+      System.out.println(Duration.between(start, end));
+    }
+  }
+
+  private static void runIt(final Path file) throws IOException {
+    final String input = Files.readString(file);
+    System.out.println("TODO: result goes here");
+  }
+}
